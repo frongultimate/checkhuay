59958761e523556418e4b234300e9db7f6b830ef
frances98 <frongultimate@gmail.com>
2023-05-07 22:46:33 +0700

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Cards from "./components/Cards";
 import Navbar from "./components/Navbar";
-
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>Check Huay Online</title>
+      </Head>
       <Navbar />
       <Cards />
     </main>
-  )
+  );
 }
